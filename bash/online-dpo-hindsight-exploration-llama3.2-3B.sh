6e8548888f6d0ad2ec4ec62abbe06d60f688a327
Yuki Wang <yukiluna1999@gmail.com>
2025-05-09 22:54:17 -0400

    copied exploration dpo bash script

---
+++ bash/online-dpo-hindsight-exploration-llama3.2-3B.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+TRAIN_SPLITS=train_10k
+TEST_SPLITS=val
+TRAIN_EPOCHS=1
+
+# Default values
+TRAIN_BATCH_SIZE=2
+GRAD_ACC=6 # When we use 4 GPUs (3 to train, 1 to generate responses)
+NUM_PROCESSES=3 # When we use 4 GPUs
+VLLM_DEVICE="cuda:3"
+
+
+########################################################################################
+# Checklist
+# - Verify DATA_DIR
+# - Verify MODEL
+# - Verify MODEL_LOG_NAME
+########################################################################################
+
+DOMAIN=twenty_questions
+SAVE_FREQ=55
+MAX_SEQ_LENGTH=2000
+OUTPUT_LENGTH=256
+
+
+current_date=$(date +"%y%m%d_%H%M%S")
+
+# Default values
+NOTE=""
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --note)
+            NOTE="_$2"
+            shift
+            ;;
+    esac
+    shift
+done
+
+echo "Note: $NOTE"
+
+export TRITON_CACHE_DIR=/share/portal/hw575
+TRITON_CACHE_DIR=/share/portal/hw575/.triton
+
+DATASET=data/${DOMAIN}/prm/${DATA_DIR}
+SAVE_DIR=save/${DOMAIN}/online_dpo/${current_date}_${DATA_DIR}_${MODEL_LOG_NAME}${NOTE}/
+
+echo "Save directory: $SAVE_DIR"
+
+accelerate launch  --num-processes ${NUM_PROCESSES} \
+    --config_file configs/ds_configs/deepspeed_zero2.yaml scripts/train/online_dpo_vllm_thread.py \
+    --dataset_mixer "{\"${DATASET}\": 1.0}" \
+    --dataset_train_splits ${TRAIN_SPLITS} \
+    --dataset_eval_mixer "{\"${DATASET}\": 1.0}" \
+    --dataset_eval_splits ${TEST_SPLITS} \
+    --domain_name ${DOMAIN} \
+    --model_name_or_path ${POLICY_MODEL} \
+    --reward_model_path ${REWARD_MODEL} \
+    --non_stop_penalty \
+    --penalty_reward_value -10.0 \
+    --stop_token eos \
+    --learning_rate 8e-8 \
+    --total_episodes 10000 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps ${GRAD_ACC} \
+    --gradient_checkpointing True \
+    --max_prompt_token_length ${MAX_SEQ_LENGTH} \
+    --response_length ${OUTPUT_LENGTH} \
+    --min_response_length 1 \
+    --num_train_epochs ${TRAIN_EPOCHS} \
+    --beta 0.03 \
+    --temperature 0.7 \
+    --num_generation_per_prompt 2 \
+    --sanity_check_max_samples 128 \
+    --output_dir ${SAVE_DIR} \
+    --checkpoint_output_dir tmp/chkpts/ \
+    --save_freq ${SAVE_FREQ} \
+    --vllm_device ${VLLM_DEVICE} \
+    --vllm_gpu_memory_utilization 0.9 \
+    --hf_metadata_dataset "" \
+    --no_try_launch_beaker_eval_jobs \
+    --gradient_checkpointing \
+    --wandb_project_name "LLM_RM" \
+    --with_tracking
